f389b33717049250cdfe3971a28b8b9046a38771
DMillan96 <danmil114@gmail.com>
2025-04-19 09:47:43 -0400

    Java Springboot setup

---
+++ src/test/java/com/github/dmillan/apps_api/AppsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dmillan.apps_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
